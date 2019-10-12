b0ff8557d94f14cae3f13e1ec8c88bc581d026f8
Taiba Aljanabi <taibaaljanabi2001@gmail.com>
2019-10-11 20:15:47 -0600

    commit

--- src/App.js
+++ src/App.js
@@ -5,6 +5,18 @@ import RecipeList from './Components/RecipeList'
 import RecipeDetails from './Components/RecipeDetails'
 
 export default class App extends Component {
+  state={
+    recipes : [],
+    url : 'https://www.food2fork.com/api/search?key=08205ce5e66a94eabe16ae3e402ea812&q'
+  }
+  async  getRecipes(){
+    const data =  await fetch(this.state.url)
+    const jsonData = await data.json()
+    this.setState({
+      recipes : jsonData.recipes
+    })
+  } 
+
   render() {
     return (
       <React.Fragment>
