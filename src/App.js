5c95b1952d6690e375992871b9a860a961cc53b6
Taiba Aljanabi <taibaaljanabi2001@gmail.com>
2019-10-16 17:10:17 -0600

    commit

--- src/App.js
+++ src/App.js
@@ -1,38 +1,38 @@
 import React, { Component } from 'react';
 import './App.css' 
-// import {recipes} from './tempList'
+import {recipes} from './tempList'
 import RecipeList from './Components/RecipeList'
 import RecipeDetails from './Components/RecipeDetails'
 
 export default class App extends Component {
   state={
-    recipes : [],
+    recipes : recipes,
     url : 'https://www.food2fork.com/api/search?key=08205ce5e66a94eabe16ae3e402ea812&q'
   }
 
 
 
-  async  getRecipes(){
-    try{
-      const data =  await fetch(this.state.url)
-      const jsonData = await data.json()
-      this.setState({
-        recipes : jsonData.recipes
-      })
+  // async  getRecipes(){
+  //   try{
+  //     const data =  await fetch(this.state.url)
+  //     const jsonData = await data.json()
+  //     this.setState({
+  //       recipes : jsonData.recipes
+  //     })
     
-    }catch(error){
-     console.log(error)
-    }
-  }
-  componentDidMount(){
-    this.getRecipes()
-  } 
+  //   }catch(error){
+  //    console.log(error)
+  //   }
+  // }
+  // componentDidMount(){
+  //   this.getRecipes()
+  // } 
 
   render() {
     console.log(this.state.recipes)
     return (
       <React.Fragment>
-       <RecipeList/>
+       <RecipeList recipes= {this.state.recipes}/>
        <RecipeDetails/>
       </React.Fragment>
     );
